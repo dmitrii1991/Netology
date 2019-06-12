688d1e71b620f63257657e7fec78198b465e968e
hackerrank.com <44140614+dmitrii1991@users.noreply.github.com>
2019-06-12 03:32:15 +0300

    Create homework2.py

---
+++ ADVANCED_PYTHON/LESSON 1.3/homework2.py
@@ -0,0 +1,21 @@
+def path_logg(path):
+    import os
+    logs_logger = os.path.join(path, 'INFO.txt')
+    def logger(function):
+        def function_time(*args, **kwargs):
+            import datetime
+            time_now = datetime.datetime.today().strftime("%d.%m.%Y.%H:%M:%S")
+            result = function(*args, **kwargs)
+            with open(logs_logger, 'w', encoding="utf-8") as info:
+                print('Time=', time_now, 'Name function=', function.__name__, 'Result =', result, 'Arguments =',
+                      *args,**kwargs, file=info)
+            return result
+        return function_time
+    return logger
+
+@path_logg('')
+def ret(number):
+    f = number**3
+    return f
+
+ret(10)
