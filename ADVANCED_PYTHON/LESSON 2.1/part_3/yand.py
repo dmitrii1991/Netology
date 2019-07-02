7ff16062a16e15ea7a61c1c940feae3d0e72ddbd
hackerrank.com <44140614+dmitrii1991@users.noreply.github.com>
2019-07-02 22:18:39 +0300

    Update yand.py

--- ADVANCED_PYTHON/LESSON 2.1/part_3/yand.py
+++ ADVANCED_PYTHON/LESSON 2.1/part_3/yand.py
@@ -10,21 +10,33 @@ class Yandex:
     def auto(self):
         browser = webdriver.Chrome()  # подключаем указатель на браузер
         browser.get('https://passport.yandex.ru/auth/')  # переход на страницу
+        time.sleep(1)
+
         # заполнение поля пользователя
         email = browser.find_element_by_id("passp-field-login")
         email.send_keys(self.login)
+
         # нажимаем на кнопку ввести логин
         submit_email = browser.find_element_by_xpath('//button[@class="control button2 button2_view_classic button2_size_l button2_theme_action button2_width_max button2_type_submit passp-form-button"]')
         submit_email.click()
         time.sleep(2)
+
         # заполнение пароль
         login = browser.find_element_by_id('passp-field-passwd')
         login.send_keys(self.password)
+
         # нажимаем на кнопку ввести ПАРОЛЬ
         submit_login = browser.find_element_by_xpath('//button[@class="control button2 button2_view_classic button2_size_l button2_theme_action button2_width_max button2_type_submit passp-form-button"]')
         submit_login.click()
+        time.sleep(3)
+
+        #Получаем данные пользователя
+        displaylogin = browser.find_element_by_class_name('personal-info-login__displaylogin')
+        print(displaylogin.text)
+        return displaylogin.text
+
 
 if __name__ == "__main__":
     
-    Dima = Yandex('maksimus.maksi', '1ddsd')
-    Dima.auto()
+    dima = Yandex('maksimus.maksi', '1991199')
+    dima.auto()
