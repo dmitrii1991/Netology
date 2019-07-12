424534d7b2be2e51de6530229af3ad79d0e947c1
hackerrank.com <44140614+dmitrii1991@users.noreply.github.com>
2019-07-12 03:59:19 +0300

    Update hw.py

--- ADVANCED_PYTHON/LESSON 2.4/hw.py
+++ ADVANCED_PYTHON/LESSON 2.4/hw.py
@@ -2,10 +2,7 @@ from datetime import datetime
 import csv
 import re
 from pymongo import MongoClient
-
-client = MongoClient()
-db = client.tickets
-
+from pprint import pprint
 
 def read_data(csv_file, db):
     """
@@ -14,16 +11,65 @@ def read_data(csv_file, db):
     with open(csv_file, encoding='utf8') as csvfile:
         # прочитать файл с данными и записать в коллекцию
         reader = csv.DictReader(csvfile)
-        tickets_data = []
+        tikets_list = []
         for line in reader:
-            line['Цена'] = int(line['Цена'])
-            date_str = line['Дата'] + f'.{datetime.now().year}'
-            tickets_data.append(line)
-        concert_tickets = db.concert
-        concert_tickets.insert_many(tickets_data)
+            tikets_dict = {
+                'Исполнитель': line['Исполнитель'],
+                'Цена': int(line["Цена"]),
+                'Место': line["Место"],
+                'Дата': datetime.strptime(line["Дата"] + '.2019', '%d.%m.%Y')
+            }
+            tikets_list.append(tikets_dict)
+        db.insert_many(tikets_list)
+        return list(db.find())
 
+def find_cheapest(db):
+    """
+    Отсортировать билеты из базы по возрастания цены
+    Документация: https://docs.mongodb.com/manual/reference/method/cursor.sort/
+    """
+    cheapest= db.aggregate(
+        [
+            {'$sort': {'Цена': 1}}
+        ]
+    )
+    return list(cheapest)
+
+def find_by_name(name, db):
+    """
+    Найти билеты по имени исполнителя (в том числе – по подстроке),
+    и вернуть их по возрастанию цены
+    """
+    find_name = re.escape(name)
+    regex = re.compile(find_name)
+    result = db.find({'Исполнитель': regex })
+    return list(result)
+
+
+def find_by_date(date, db):
+    """
+     Cортировкa по дате мероприяти
+     """
+    start = datetime.strptime('01.' + date, '%d.%m.%Y')
+    finish = datetime.strptime('31.' + date, '%d.%m.%Y')
+    find_date = db.find(
+             {'Дата':
+                 {
+                 '$gte': start,
+                 '$lte': finish,
+                 }
+             }
+    )
+    return list(find_date)
 
 
 if __name__ == '__main__':
-    read_data('artists.csv', db)
 
+    client = MongoClient()
+    db = client.db
+    tikets = db.tikets
+
+    pprint(read_data('artists.csv', tikets))
+    pprint(find_cheapest(tikets))
+    pprint(find_by_name('T-Fest', tikets))
+    pprint(find_by_date('12.2019', tikets))
