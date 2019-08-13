4d5c36960b8096a4e3cbd7b46db9bbeb123c9d1d
hackerrank.com <44140614+dmitrii1991@users.noreply.github.com>
2019-08-14 00:10:05 +0300

    Add files via upload

--- Django/lesson_1/3/app/views.py
+++ Django/lesson_1/3/app/views.py
@@ -2,6 +2,8 @@ import csv
 from .settings import BUS_STATION_CSV
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
 
 
 def index(request):
@@ -9,6 +11,8 @@ def index(request):
 
 
 def bus_stations(request):
+    page = request.GET.get("page", 1)
+    print(page)
     with open(BUS_STATION_CSV, encoding='cp1251') as file:
         reader = csv.DictReader(file)
         total_data = []
@@ -19,45 +23,25 @@ def bus_stations(request):
                 'District': raw['District']
             }
             total_data.append(data)
-        numbers_of_rows = len(total_data)
+        p = Paginator(total_data, 20)
+
+        if int(page) == 1:
+            pageDown = 1
+            pageUp = int(page) + 1
+        elif int(page) == p.num_pages:
+            pageDown = int(page) - 1
+            pageUp = p.num_pages
+        elif int(page) in p.page_range:
+            pageUp = int(page) + 1
+            pageDown = int(page) - 1
+        else:
+            return HttpResponse(f'Нет такой страницы. Всего страниц: {p.num_pages}')
         context = {
-            'prev_page_url': None,
-            'next_page_url': None,
+            'prev_page_url': f'?page={pageDown}',
+            'next_page_url': f'?page={pageUp}',
+            'current_page': int(page),
+            'bus_stations': p.page(int(page)).object_list,
         }
-        i = 1
-        show_context_data = []
 
-        while i <= round(numbers_of_rows / 20):
-            if i == 1:
-                next_page = i + 1
-                context = {
-                    'prev_page_url': None,
-                    'next_page_url': f'?page={next_page}',
-                    'current_page': 1,
-                    'bus_stations': total_data[0:20],
-                }
-                show_context_data.append(context)
-            elif i == round(numbers_of_rows / 20):
-                previous_page = i - 1
-                context = {
-                    'prev_page_url': f'?page={previous_page}',
-                    'next_page_url': None,
-                    'current_page': i,
-                    'bus_stations': total_data[previous_page * 20:i * 20],
-                }
-            else:
-                previous_page = i - 1
-                next_page = i + 1
-                context = {
-                    'prev_page_url': f'?page={previous_page}',
-                    'next_page_url': f'?page={next_page}',
-                    'current_page': i,
-                    'bus_stations': total_data[i * 10:i * 10 + 20],
-                }
-            show_context_data.append(context)
-            i += 1
-        if 'page' in request.GET:
-            return render_to_response('index.html', context=show_context_data[int(request.GET['page'])])
-        return render_to_response('index.html', context=show_context_data[1])
-
+    return render_to_response('index.html', context=context)
 
