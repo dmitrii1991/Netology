35f50cdb87c67b457f083e8d92fd137e900b34b3
Dmitry Kulinich <44140614+dmitrii1991@users.noreply.github.com>
2019-09-04 22:43:29 +0300

    Add files via upload

---
+++ Django/lesson_5/task2/app/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import redirect, render, get_object_or_404
+from django.urls import reverse
+
+# from django.http import HttpResponse
+
+from .models import Product, Review
+from .forms import ReviewForm
+
+
+def product_list_view(request):
+    template = 'app/product_list.html'
+    products = Product.objects.all()
+    context = {
+        'product_list': products,
+    }
+
+    return render(request, template, context)
+
+
+def product_view(request, pk):
+    template = 'app/product_detail.html'
+    product = get_object_or_404(Product, id=pk)
+    reviews = Review.objects.all().filter(product=product)
+    if 'reviewed_products' not in request.session:
+        request.session['reviewed_products'] = list()
+    form = ReviewForm
+    if request.method == 'POST':
+        review_ans = request.POST['text'] # чтение
+        review = Review(text=review_ans, product=product)  # запись
+        review.save()
+
+    context = {
+        'form': form,
+        'product': product,
+        'is_review_exist': True,
+    }
+
+    if product in request.session['reviewed_products']:# проверка
+        context = {
+            'form': form,
+            'product': product,
+            'reviews': reviews,
+        }
+
+    return render(request, template, context)
+xt)
