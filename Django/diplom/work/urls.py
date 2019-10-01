eb545eec9e985ecf1d48da0488e92da98e2bdc08
Dmitry Kulinich <44140614+dmitrii1991@users.noreply.github.com>
2019-10-02 00:06:18 +0300

    Add files via upload

--- Django/diplom/work/urls.py
+++ Django/diplom/work/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import smartphones, phone, accessories, index, RegisterView
-from django.contrib.auth.views import LoginView, LogoutView  # авторизация вход, выход
+from .views import smartphones, phone, accessories, index, RegisterView, LoginView
+from django.contrib.auth.views import LogoutView  # выход
 
 
 urlpatterns = [
@@ -8,8 +8,8 @@ urlpatterns = [
     path('smartphones/', smartphones, name='smartphones'),
     path('smartphones/<int:bd_id>/', phone, name='phone'),
     path('smartphones/accessories/', accessories, name='accessories'),
-    path('login/', LoginView.as_view(redirect_field_name='redirect_to'), name='auth_login'),  # авторизация вход redirect_to - переход по заданному пути
-    path('logout/', LogoutView .as_view(redirect_field_name='redirect_to'), name='logout_view'),  # авторизация выход
+    path('login/', LoginView.as_view(), name='auth_login'),  # авторизация вход redirect_to - переход по заданному пути
+    path('logout/', LogoutView.as_view(redirect_field_name='redirect_to'), name='logout_view'),  # авторизация выход
     path('signup/',  RegisterView.as_view(), name='signup'),
     #
     # path('logout/', auth.views.logout_view, name='logout_view'),
--- Django/diplom/work/views.py
+++ Django/diplom/work/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render, redirect
 from django.urls import reverse  # использов. тега шаблона url
 from django.views.generic import TemplateView  # Рендер шаблона/классы-представления
 from django.contrib.auth.models import User  # доступ к пользователям
-from django.core.exceptions import ObjectDoesNotExist #
+from django.core.exceptions import ObjectDoesNotExist  # подключение ошибок
+from django.contrib.auth import authenticate, login  # доступ к регистрации
 
 from .models import Bd
 
 import random
+
 
 def smartphones(request):
     template = 'work/smartphones.html'
@@ -15,16 +17,19 @@ def smartphones(request):
     context = {'phones': phones}
     return render(request, template, context)
 
+
 def phone(request, bd_id):
     template = 'work/phone.html'
     phone = Bd.objects.get(pk=bd_id)
     context = {'phone': phone}
     return render(request, template, context)
 
+
 def accessories(request):
     template = 'work/empty_section.html'
     context= {}
     return render(request, template, context)
+
 
 def index(request):
     template = 'work/index.html'
@@ -34,22 +39,48 @@ def index(request):
     context = {'phones': choices}
     return render(request, template, context)
 
+
+class LoginView(TemplateView):
+    template_name = "registration/login.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        context = {}
+        if request.method == 'POST':
+            email = request.POST['email']
+            password = request.POST['password']
+            # поиск пользователя по почте
+            try:
+                user_ = User.objects.get(email=email)
+            except ObjectDoesNotExist:
+                pass
+            # Авторизация
+            user = authenticate(request, username=user_.username, password=password)
+            if user:
+                login(request, user)
+                return redirect("/prosto/")
+            else:
+                context['error'] = "Логин или пароль неправильные"
+        return render(request, self.template_name, context)
+
+
 class RegisterView(TemplateView):
     template_name = "registration/signup.html"
 
     def dispatch(self, request, *args, **kwargs):
+        context = {}
         if request.method == 'POST':
             username = request.POST.get('username')
             email = request.POST.get('email')
             password = request.POST.get('password')
             password2 = request.POST.get('password2')
-
+            # Провекрка повторную регистрацию
             if password == password2:
                 try:
-                    _ = User.objects.get(username='1112')
+                    _ = User.objects.get(username=username)
                 except ObjectDoesNotExist:
-                    return render(request, self.template_name)
-                else:
                     User.objects.create_user(username, email, password)  # создание пользователя
                     return redirect(reverse("auth_login"))  # возврат на авторизацию
+                else:
+                    context['error'] = "Такой поользователь есть"
 
+        return render(request, self.template_name, context)
